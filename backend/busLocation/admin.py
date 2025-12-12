e7fa458e2535e07891a3002f3599644b870c2d81
beknazaarr <beknazaarr@gmail.com>
2025-12-12 17:01:17 +0600

    Админ для buslocation

--- backend/busLocation/admin.py
+++ backend/busLocation/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import BusLocation
 
-# Register your models here.
+
+@admin.register(BusLocation)
+class BusLocationAdmin(admin.ModelAdmin):
+    list_display = ('bus', 'shift', 'latitude', 'longitude', 'speed', 'timestamp')
+    list_filter = ('timestamp', 'bus')
+    search_fields = ('bus__registration_number', 'shift__driver__username')
+    readonly_fields = ('timestamp',)
+    
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('bus', 'shift', 'timestamp')
+        }),
+        ('Координаты', {
+            'fields': ('latitude', 'longitude', 'accuracy')
+        }),
+        ('Движение', {
+            'fields': ('speed', 'heading')
+        }),
+    )
