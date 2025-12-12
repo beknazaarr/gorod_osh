630afff2cc41a42971b0165708e73f5ecadfa079
beknazaarr <beknazaarr@gmail.com>
2025-12-12 16:58:31 +0600

    Админ для user

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'role', 'phone_number', 'is_blocked', 'is_active', 'created_at')
+    list_filter = ('role', 'is_blocked', 'is_active')
+    search_fields = ('username', 'first_name', 'last_name', 'phone_number')
+    
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Дополнительная информация', {
+            'fields': ('role', 'phone_number', 'is_blocked')
+        }),
+    )
+    
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        ('Дополнительная информация', {
+            'fields': ('role', 'phone_number')
+        }),
+    )
